0d250a3839a27cb4c6d8870e6abd3bef940afcda
betay1 <betelhemaynalem0@gmail.com>
2023-11-29 11:50:15 -0800

    0x00 python task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - chck linked list if it  contains a cycle
+ * @list: linked list
+ * Return: 1 if the list has a cycle, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+        listint_t *slow = list;
+        listint_t *fast = list;
+
+        if (!list)
+                return (0);
+        while (slow && fast && fast->next)
+        {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+                return (1);
+        }
+        return (0);
+}
