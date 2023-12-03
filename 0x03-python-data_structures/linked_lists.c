41b5077ee71e4adf2fe4472b642c7d6ccd6682c3
betay1 <betelhemaynalem0@gmail.com>
2023-12-03 05:35:32 -0800

    0x03 python task

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -9,19 +9,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+const listint_t *current;
+unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-
-    return (n);
+current = h;
+n = 0;
+while (current != NULL)
+	{
+	printf("%i\n", current->n);
+	current = current->next;
+	n++;
+	}
+	return (n);
 }
 
 /**
@@ -30,15 +29,16 @@ size_t print_listint(const listint_t *h)
  * @n: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new;
-    listint_t *current;
+	listint_t *new;
+	listint_t *current;
 
-    current = *head;
+	current = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
