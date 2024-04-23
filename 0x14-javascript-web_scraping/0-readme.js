e8c8022441910bd7b10ba4e1bb5848265f68894d
betay1 <betelhemaynalem0@gmail.com>
2024-04-23 12:49:21 -0700

    java script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-// Import the built-in Node.js 'fs' module.
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-// Use fs.readFile() to read the contents of a file specified as a command-line argument
-// 'utf8' specifies the encoding of the file being read
+process.argv[0] = 'node';
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
 
-	if (error) {
-// If an error occurs during the file read operation, the 'error' parameter will contain an error object.
-	console.error('Error reading the file:', error);
-
-	} else {
-// If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
-	console.log(content);
-	}
-});
+if (process.argv.length === 3) {
+  const filePath = process.argv[2];
+  readFile(filePath);
+}
